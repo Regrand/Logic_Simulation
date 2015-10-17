2def7b334ac36aa7d97a35303f5b8d23df0750b3
Aniket <aniket.deshmukh@iitb.ac.in>
2015-10-17 23:08:33 +0530

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/* 
+Author: Aniket Deshmukh
+17th October 2015
+CAD Project: Logic Simulation
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+// Global signal matrix
+vector<int> signal(MAX_NUM_NODES);
+
+ 
+int main(int argc, char** argv) {
+
+	// Number of gates
+	int NUM_NODES = 10;
+	vector<vector<int> > adj_matrix(NUM_NODES, std::vector<int>(NUM_NODES));
+	vector<node> gates(NUM_NODES);
+	
+	/* 
+	Define adjacency matrix here 
+	*/
+	
+	// Set input nodes and initial conditions
+	
+	
+	// Begin simulation
+	
+	
+
+	return 0;
+}
+	
